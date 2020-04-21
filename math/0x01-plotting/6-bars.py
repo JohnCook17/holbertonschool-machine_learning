6bd717aacab42ad783218eac8413c311d7336b19
JohnCook17 <jcook0017@gmail.com>
2020-04-21 06:27:16 +0000

    not my best work but it works

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -6,32 +6,30 @@ np.random.seed(5)
 fruit = np.random.randint(0, 20, (4, 3))
 
 # your code here
-print(fruit)
 fruits = ("apples", "bananas", "oranges", "peaches")
 Farrah = fruit[0:][0:1]
 Fred = fruit[0:][1:2]
 Felicia = fruit[0:][2:3]
 people = "Farrah", "Fred", "Felicia"
-print("+++++++++++++++++")
-print(Farrah)
-print(Fred)
-print(Felicia)
 my_dict = {key: value[0:] for key, value in zip([v[0:] for v in fruits], [v[0:] for v in fruit])}
-print(my_dict)
-print(my_dict["apples"])
-print([key for key in fruits])
-
-
-for key in fruits:
+i = 0
+for person in people:
     fruit_to_add = 0
-    for person in people:
+    for key in fruits:
         if key == "apples":
             color = "red"
         elif key == "bananas":
             color = "yellow"
-        print(my_dict.get(key))
-        
-        plt.bar(person, key, color=color)
-        fruit_to_add += my_dict.get(key)
-
+        elif key == "oranges":
+            color = "#ff8000"
+        elif key == "peaches":
+            color = "#ffe5b4"
+        plt.bar(person, my_dict.get(key)[i], color=color, bottom=fruit_to_add, width=0.5)
+        fruit_to_add += my_dict.get(key)[i]
+    i += 1
+plt.legend(fruits)
+plt.ylim(0, 80, 10)
+plt.ylabel("Quantity of Fruit")
+plt.xticks(people)
+plt.title("Number of Fruit per person")
 plt.show()
