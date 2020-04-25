dca89001b520a3be0da763950a0ed8c5bc4365da
JohnCook17 <jcook0017@gmail.com>
2020-04-25 19:32:06 +0000

    was not handleing 0 properly either

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,9 +6,9 @@ def summation_i_squared(n):
     """uses math to sum the squares"""
     try:
         if isinstance(n, int):
-            if n < 0:
+            if n <= 0:
                 return None
-            if n >= 0:
+            if n > 0:
                 answer = ((n ** 3) / 3) + ((n ** 2) / 2) + (n / 6)
                 return int(answer)
     except ValueError:
