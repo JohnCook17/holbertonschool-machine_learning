af7cf5e7c91328f4ca1190e0679131060339290f
JohnCook17 <jcook0017@gmail.com>
2020-04-25 19:38:49 +0000

    try except

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,10 +4,11 @@
 
 def poly_derivative(poly):
     """Finds a derivative of an equation"""
-    if [n for n in poly if type(n) == int or type(n) == float]:
+    try:
         answer = [poly[i] * i for i in range(1, len(poly))]
         if answer == 0:
             return [0]
         else:
             return answer
-    return None
+    except (ValueError, IndexError):
+        return None
