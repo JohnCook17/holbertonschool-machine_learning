8c9661e136fe4d2525a25a060ba76d0d72187494
JohnCook17 <jcook0017@gmail.com>
2020-04-25 19:50:06 +0000

    try except refactor, adds C to end

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,10 +4,15 @@
 
 def poly_integral(poly, C=0):
     """returns the result of integration of a polynomial"""
-    res = [0]
-    for i in range(len(poly)):
-        c = poly[i] / (i + 1)
-        if c % 1 == 0:
-            c = int(c)
-        res.append(c)
-    return res
+    try:
+        res = [0]
+        for i in range(len(poly)):
+            c = poly[i] / (i + 1)
+            if c % 1 == 0:
+                c = int(c)
+            res.append(c)
+        if C:
+            res.append(C)
+        return res
+    except(ValueError, IndexError):
+        return None
