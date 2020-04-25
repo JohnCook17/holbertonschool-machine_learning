22c075e8d0bfb3d87d19ea8f76f69fa6270c5d24
JohnCook17 <jcook0017@gmail.com>
2020-04-25 18:32:12 +0000

    changed how floats and ints are handled

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,13 @@
 
 def summation_i_squared(n):
     """uses math to sum the squares"""
-    if isinstance(n, int):
+    if isinstance(n, float) and n % 1 == 0:
+        n = int(n)
+        answer = ((n ** 3) / 3) + ((n ** 2) / 2) + (n / 6)
+        if answer % 1 == 0:
+            answer = int(answer)
+        return answer
+    elif isinstance(n, int):
         answer = ((n ** 3) / 3) + ((n ** 2) / 2) + (n / 6)
         if answer % 1 == 0:
             answer = int(answer)
