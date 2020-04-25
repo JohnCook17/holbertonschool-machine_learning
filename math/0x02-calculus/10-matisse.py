5df1d3c8f1242b722f30b1f3007f65c12eb2dc9e
JohnCook17 <jcook0017@gmail.com>
2020-04-25 17:58:53 +0000

    should now handle if the poly evaluates to 0

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,5 +5,9 @@
 def poly_derivative(poly):
     """Finds a derivative of an equation"""
     if [n for n in poly if type(n) is int or type(n) is float]:
-        return [poly[i] * i for i in range(1, len(poly))]
+        answer = [poly[i] * i for i in range(1, len(poly))]
+        if answer == 0:
+            return [0]
+        else:
+            return answer
     return None
