319d3d9af528a5641779e99fec765695358d7091
JohnCook17 <jcook0017@gmail.com>
2020-04-25 20:08:46 +0000

    might be handleing the constant correctly now

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,14 +5,12 @@
 def poly_integral(poly, C=0):
     """returns the result of integration of a polynomial"""
     try:
-        res = [0]
+        res = [C]
         for i in range(len(poly)):
             c = poly[i] / (i + 1)
             if c % 1 == 0:
                 c = int(c)
             res.append(c)
-        if C:
-            res.append(C)
         return res
     except(ValueError, IndexError):
         return None
