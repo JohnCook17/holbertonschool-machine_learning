1159982fb06dc163336e4d6fd75b2f82654e8bfc
JohnCook17 <jcook0017@gmail.com>
2020-05-13 20:35:47 +0000

    added the required conditonals

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -5,13 +5,19 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """One hot encodes using np"""
+    if not isinstance(Y, np.ndarray):
+        return None
+    if not isinstance(classes, int):
+        return None
     if Y.size == 0:
         return None
-    if classes < 1:
+    if classes < 2:
+        return None
+    if classes < np.max(Y):
         return None
     one_hot = []
     for value in Y:
         number = [0 for n in range(classes) if isinstance(n, int)]
         number[value] = 1
         one_hot.append(number)
-    return np.array(one_hot, dtype=float).T
+    return np.array(np.array(one_hot, dtype=float).T)
