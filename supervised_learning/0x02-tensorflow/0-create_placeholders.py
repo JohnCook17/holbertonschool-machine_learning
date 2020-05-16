db420b3ffdef87d1e62ed535f412095919cc93d0
JohnCook17 <jcook0017@gmail.com>
2020-05-16 17:24:06 +0000

    forgot to name placeholders

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 def create_placeholders(nx, classes):
     """x is the input data placeholder,
     y is for the onehot lables for input data"""
-    x = tf.placeholder(float, [None, nx])
-    y = tf.placeholder(float, [None, classes])
+    x = tf.placeholder(float, [None, nx], name="x")
+    y = tf.placeholder(float, [None, classes], name="y")
     return x, y
