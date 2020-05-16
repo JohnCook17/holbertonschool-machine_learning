21521539ba6be5c1c0c496594a487da8fbb73184
JohnCook17 <jcook0017@gmail.com>
2020-05-16 17:26:00 +0000

    returns save path now instead of save object

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -59,4 +59,4 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
             iteration += 1
         save = saver.save(sess, save_path)
-        return save
+        return save_path
