a682e8c8338f4df65d95ef751b33467ddc6f6c21
JohnCook17 <jcook0017@gmail.com>
2020-05-21 21:12:55 +0000

    documentation

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""Exponential weighted moving average with bias correction"""
 import numpy as np
 
 
 def moving_average(data, beta):
-    """"""
+    """Corrects for bias in an Exponential weighted moving average"""
     vt = 0
     ewma = []
     for i in range(0, (len(data))):
