bcc67bb5d8c12d2916cef1397489356dc63fc432
JohnCook17 <jcook0017@gmail.com>
2020-05-21 20:35:02 +0000

    works now, after a peer review

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -5,10 +5,10 @@ import numpy as np
 
 def moving_average(data, beta):
     """"""
-    ewma = [data[0]]
-    for i in range(1, (len(data))):
-        bt = (1 - beta ** i)
-        vt = data[i] / bt
-        print(vt)
-        ewma.append((beta * ewma[i - 1]) + ((1 - beta) * vt))
+    vt = 0
+    ewma = []
+    for i in range(0, (len(data))):
+        bt = (1 - beta ** (i + 1))
+        vt = ((beta * vt) + ((1 - beta) * data[i]))
+        ewma.append(vt / bt)
     return ewma
