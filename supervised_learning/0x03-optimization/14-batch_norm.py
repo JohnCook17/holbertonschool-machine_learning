621152ba35810e979a2554e3564a8805591492e8
JohnCook17 <jcook0017@gmail.com>
2020-05-23 22:44:36 +0000

    activating layres after norm now

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -6,12 +6,12 @@ import tensorflow as tf
 def create_batch_norm_layer(prev, n, activation):
     """prev is the previous layer, n is the shape of the new layer,
     and activation is the activation"""
+    i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, activation=None, kernel_initializer=i)
     beta = tf.Variable(tf.zeros([n]), trainable=True)
     gamma = tf.Variable(tf.ones([n]), trainable=True)
-    i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    input_t = tf.layers.Dense(units=n, activation=activation,
-                              kernel_initializer=i)(prev)
-    mean, variance = tf.nn.moments(input_t, [0])
-    return tf.nn.batch_normalization(input_t, mean=mean, variance=variance,
+    mean, variance = tf.nn.moments(layer(prev), axes=[0])
+    norm = tf.nn.batch_normalization(layer(prev), mean=mean, variance=variance,
                                      offset=beta, scale=gamma,
                                      variance_epsilon=1e-8)
+    return activation(norm)
