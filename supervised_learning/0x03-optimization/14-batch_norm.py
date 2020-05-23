9b2d81d0d3c3359e8127dd4c5b3ba842cda39c03
JohnCook17 <jcook0017@gmail.com>
2020-05-23 23:06:20 +0000

    trailing white space

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -10,7 +10,7 @@ def create_batch_norm_layer(prev, n, activation):
     layer = tf.layers.Dense(units=n, activation=None, kernel_initializer=i)
     mean, variance = tf.nn.moments(layer(prev), axes=[0])
     beta = tf.Variable(tf.zeros([n]), trainable=True)
-    gamma = tf.Variable(tf.ones([n]), trainable=True) 
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
     norm = tf.nn.batch_normalization(layer(prev), mean=mean, variance=variance,
                                      offset=beta, scale=gamma,
                                      variance_epsilon=1e-8)
