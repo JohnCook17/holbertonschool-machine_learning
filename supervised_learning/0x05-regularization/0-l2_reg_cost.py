64cf1a13524ba15a3ca45aaf6f60523fceff6592
JohnCook17 <jcook0017@gmail.com>
2020-05-27 23:35:02 +0000

    submitting for peer review, floating point error or pemdos error

---
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""""""
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """"""
+    new_weights = []
+    for layer in range(1, L + 1):
+        weight = weights["W" + str(layer)]
+        weight = np.linalg.norm(weight, ord="fro")
+        new_weights.append(weight)
+    new_weights = np.asarray(new_weights)
+    return np.sum(cost, keepdims=True) + ((lambtha / (2 * m)) * np.sum(new_weights, keepdims=True) ** 2) / m
