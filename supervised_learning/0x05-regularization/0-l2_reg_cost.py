9db9e384d04154a3f02f9eac31a46c0432244f79
JohnCook17 <jcook0017@gmail.com>
2020-05-29 02:24:46 +0000

    should be ready for the checker

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-""""""
+"""l2 forward propagation"""
 import numpy as np
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
-    """"""
+    """cost is the cost of the neural network, lambtha is trainable perameter
+    L is the number of layers of the network, and m is the number of inputs"""
     new_weights = []
     for layer in range(1, L + 1):
         weight = weights["W" + str(layer)]
         weight = np.linalg.norm(weight, ord="fro")
         new_weights.append(weight)
     new_weights = np.asarray(new_weights)
-    return np.sum(cost, keepdims=True) + ((lambtha / (2 * m)) * np.sum(new_weights, keepdims=True))
+    return np.sum(cost, keepdims=True) + ((lambtha / (2 * m)) *
+                                          np.sum(new_weights, keepdims=True))
--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
-""""""
+"""performes l2 regularization gradeint descent"""
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """"""
+    """Y is the one hot labels. weights is a dict. with weights and biases
+    cache contains the output of each layer. lambtha is a traning perameter.
+    L is the number of layers."""
     m = Y.shape[1]
-    new_weights = {}
-    # print(L)
-    for layer in (L, 0, -1):
-        print(layer)
+    new_weights = weights
+    for layer in range(L, 0, -1):
         if layer == L:
             # last layer is softmax
             dz = cache["A" + str(layer)] - Y
         else:
             # other layers are tanh
-            dz = 1 - cache["A" + str(layer)] ** 2
-        # print(dz.shape)
-        l2_w = (lambtha / m) * weights["W" + str(layer)]
-        l2_b = (lambtha / m) * weights["b" + str(layer)]
-        # print(l2_w.shape)
-        # print(cache["A" + str(layer)].shape)
-        dw = np.matmul(dz, cache["A" + str(layer)].T) + l2_w
-        db = np.matmul(dz, cache["A" + str(layer)].T) + l2_b
+            dz = da * (1 - cache["A" + str(layer)] ** 2)
+        l2_w = (lambtha / m) * new_weights["W" + str(layer)]
+        l2_b = (lambtha / 2 * m) * new_weights["b" + str(layer)] ** 2
+        dw = np.matmul(dz, cache["A" + str(layer - 1)].T) + l2_w
+        db = np.sum(dz, axis=1, keepdims=True)
+        da = np.matmul(new_weights["W" + str(layer)].T, dz)
         # set new weights
-        new_weights["W" + str(layer)] = weights["W" + str(layer)] - alpha * dw
-        new_weights["b" + str(layer)] = weights["b" + str(layer)] - alpha * db
+        new_weights["W" + str(layer)] = (new_weights["W" + str(layer)] -
+                                         alpha * dw)
+        new_weights["b" + str(layer)] = (new_weights["b" + str(layer)] -
+                                         alpha * db)
     weights = new_weights
