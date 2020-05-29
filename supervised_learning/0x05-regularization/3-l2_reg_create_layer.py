bdfac744981dab13be3b58a91c52159b0b76a916
JohnCook17 <jcook0017@gmail.com>
2020-05-29 19:35:28 +0000

    documentation

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""l2 initalization of weights for a new layer"""
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """"""
+    """uses he et al for init weight value, then uses l2 normalization"""
     i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     reg = tf.contrib.layers.l2_regularizer(scale=lambtha)
     new_layer = tf.layers.Dense(units=n, activation=activation,
