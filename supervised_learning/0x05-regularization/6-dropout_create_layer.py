caed064b12655899983b7e1606653addebd4c153
JohnCook17 <jcook0017@gmail.com>
2020-05-29 21:58:50 +0000

    realized that Dropout is a wrapper

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -7,6 +7,6 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     """"""
     i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     new_layer = tf.layers.Dense(units=n, activation=activation,
-                                kernel_initializer=i)
-    dropout = tf.layers.Dropout(new_layer, keep_prob)(prev)
+                                kernel_initializer=i)(prev)
+    dropout = tf.layers.Dropout(rate=keep_prob)(new_layer)
     return dropout
