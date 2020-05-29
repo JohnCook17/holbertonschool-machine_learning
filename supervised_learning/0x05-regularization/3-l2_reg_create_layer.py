2cb346b2ba82ee5acd322cf21a9dc8679df9ac3f
JohnCook17 <jcook0017@gmail.com>
2020-05-29 19:33:36 +0000

    init weights now

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -5,7 +5,9 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """"""
+    i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     reg = tf.contrib.layers.l2_regularizer(scale=lambtha)
     new_layer = tf.layers.Dense(units=n, activation=activation,
+                                kernel_initializer=i,
                                 kernel_regularizer=reg)
     return new_layer(prev)
