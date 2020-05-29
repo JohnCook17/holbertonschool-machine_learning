7726f5a7a3bb1debbc37c4bb268b1f687f435c43
JohnCook17 <jcook0017@gmail.com>
2020-05-29 22:01:18 +0000

    documentation

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""Creates a new layer in tf with dropout"""
 import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
-    """"""
+    """Creating a new layer with dropout"""
     i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     new_layer = tf.layers.Dense(units=n, activation=activation,
                                 kernel_initializer=i)(prev)
