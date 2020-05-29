edb187dd0a20240e2d5630f517eed9f3f68005b9
JohnCook17 <jcook0017@gmail.com>
2020-05-29 19:20:25 +0000

    now using Dense instead of dense

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -6,5 +6,6 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """"""
     reg = tf.contrib.layers.l2_regularizer(scale=lambtha)
-    return tf.layers.dense(inputs=prev, units=n, activation=activation,
-                           kernel_regularizer=reg)
+    new_layer = tf.layers.Dense(units=n, activation=activation,
+                                kernel_regularizer=reg)
+    return new_layer(prev)
