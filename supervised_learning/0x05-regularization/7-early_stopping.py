c5120454a291473a7af105562afc8711326a1c52
JohnCook17 <jcook0017@gmail.com>
2020-05-30 05:17:35 +0000

    forgot documentation

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""""""
+"""A algorithm for stopping early"""
 
 
 def early_stopping(cost, opt_cost, threshold, patience, count):
-    """"""
+    """logic for determining if we should stop early"""
     if opt_cost - cost <= threshold:
         count += 1
         if count < patience:
