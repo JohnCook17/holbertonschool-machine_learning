3691893bf087da13d66f7bc062930408995f3868
JohnCook17 <jcook0017@gmail.com>
2020-06-02 17:35:12 +0000

    fixed last layer, no dropout now, fixed keep_prob

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -13,6 +13,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         model.add(K.layers.Dense(units=layer, activation=activation,
                                  kernel_regularizer=K.regularizers.l2(lambtha)
                                  ))
-        model.add(K.layers.Dropout(rate=keep_prob, noise_shape=layer))
+        if activation != "softmax":
+            model.add(K.layers.Dropout(rate=(1 - keep_prob), noise_shape=layer))
     model.build((None, nx))
     return model
