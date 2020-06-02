2190d9254d0fa59f51fc80e8a00978c7afa9d732
JohnCook17 <jcook0017@gmail.com>
2020-06-02 17:12:09 +0000

    README.md, and docs and line breaks

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-""""""
+"""Builds a model in keras"""
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """"""
+    """Builds a model with nx being the input shape. layers is the shape of
+    the layers. activations being the activation to use per layer.
+    lambtha is the l2 regularization. keep_prob is the probability to keep
+    a neuron."""
     model = K.Sequential()
     for layer, activation in zip(layers, activations):
         model.add(K.layers.Dense(units=layer, activation=activation,
-                                 kernel_regularizer=K.regularizers.l2(lambtha)))
+                                 kernel_regularizer=K.regularizers.l2(lambtha)
+                                 ))
         model.add(K.layers.Dropout(rate=keep_prob, noise_shape=layer))
     model.build((None, nx))
     return model
