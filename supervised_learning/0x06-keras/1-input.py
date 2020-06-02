1d1192e654a06b0fa138c553f12902aace25893c
JohnCook17 <jcook0017@gmail.com>
2020-06-02 19:32:32 +0000

    docs and line breaks

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-""""""
+"""Uses keras.Model to make a model"""
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """"""
+    """nx is the size of the input, layers is a list of the
+    size of the layres, activations is a list of which activation
+    to use, lambtha is the l2 regularization, keep prob is the
+    dropout rate"""
     inputs = K.Input(shape=(nx,))
     x = inputs
     for layer, activation in zip(layers, activations):
-        x = K.layers.Dense(units=layer, activation=activation, kernel_regularizer=K.regularizers.l2(lambtha))(x)
+        x = K.layers.Dense(units=layer, activation=activation,
+                           kernel_regularizer=K.regularizers.l2(lambtha))(x)
         if activation != "softmax":
-            x = K.layers.Dropout(rate=(1 - keep_prob),noise_shape=(layer,))(x)
+            x = K.layers.Dropout(rate=(1 - keep_prob), noise_shape=(layer,))(x)
     outputs = x
     return K.Model(inputs=inputs, outputs=outputs)
