db8408e599e980a778ed8c50727c86914bc52328
JohnCook17 <jcook0017@gmail.com>
2020-06-02 20:37:18 +0000

    docs

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-""""""
+"""Uses .fit to train the network"""
 
 
 def train_model(network, data, labels, batch_size, epochs, verbose=True,
                 shuffle=False):
-    """"""
+    """network is the network to train, data is the data, labels is the data
+    labels, batch size is the size of the minibatch, epochs is the number of
+    epochs to run, verbose is print of not the progress, shuffle is to shuffle
+    the data or not"""
     return network.fit(x=data, y=labels, epochs=epochs, batch_size=batch_size,
                        verbose=verbose, shuffle=shuffle)
