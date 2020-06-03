0adde91f2b82f839f2fb806c75fec8729e5ad965
JohnCook17 <jcook0017@gmail.com>
2020-06-03 17:45:08 +0000

    same as 4, but with validation data

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Uses .fit to train the network"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """network is the network to train, data is the data, labels is the data
+    labels, batch size is the size of the minibatch, epochs is the number of
+    epochs to run, verbose is print of not the progress, shuffle is to shuffle
+    the data or not, now uses validation data as well"""
+    return network.fit(x=data, y=labels, validation_data=validation_data,
+                       epochs=epochs, batch_size=batch_size,
+                       verbose=verbose, shuffle=shuffle)
