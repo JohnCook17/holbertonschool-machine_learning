d8af12bfdda6e3f96390f252a8bd8f713dc8376b
JohnCook17 <jcook0017@gmail.com>
2020-06-03 17:51:20 +0000

    modified if

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -9,7 +9,7 @@ def train_model(network, data, labels, batch_size, epochs,
     labels, batch size is the size of the minibatch, epochs is the number of
     epochs to run, verbose is print of not the progress, shuffle is to shuffle
     the data or not, now uses validation data as well"""
-    if validation_data:
+    if validation_data is not None:
         return network.fit(x=data, y=labels, validation_data=validation_data,
                            epochs=epochs, batch_size=batch_size,
                            verbose=verbose, shuffle=shuffle)
