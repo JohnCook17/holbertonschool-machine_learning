9cd00fc673c703488e07b5af90e5ae6089079ee3
JohnCook17 <jcook0017@gmail.com>
2020-06-04 17:21:32 +0000

    12-test.py

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Test a model in keras"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Test a model in keras, network is the network to test
+    data is the input data, labels are the correct labels of the
+    data, verbose is whether to print or not"""
+    history = network.fit(data, labels, verbose)
+    return history.history["loss"], history.history["acc"]
