470b456bc871011194de0af682412060e4423ddc
JohnCook17 <jcook0017@gmail.com>
2020-06-05 17:41:59 +0000

    now starting loop range properly

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,15 +9,15 @@ def convolve_grayscale_valid(images, kernel):
     the filter to apply to these images and is the shape of, kernel height
     kernel width. d1 is how many pixels out from the center of the kernel in
     dimension 1, d2 is how many pixels out from the center of the kernel in
-    dimension 2. imgs is the number of images."""
+    dimension 2. i is the number of images."""
     d1 = kernel.shape[0] // 2
     d2 = kernel.shape[1] // 2
-    imgs = images.shape[0]
+    i = images.shape[0]
     new_array_w = (images.shape[1] - (2 * d1))
     new_array_h = (images.shape[2] - (2 * d2))
-    new_array = np.empty((imgs, new_array_w, new_array_h))
-    for i_index in range(1, new_array.shape[1]):
-        for j_index in range(1, new_array.shape[2]):
+    new_array = np.empty((i, new_array_w, new_array_h))
+    for i_index in range(d1, new_array.shape[1]):
+        for j_index in range(d2, new_array.shape[2]):
             i_start = i_index - d1
             i_end = i_index + d1 + 1
             j_start = j_index - d2
