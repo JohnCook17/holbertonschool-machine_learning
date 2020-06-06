6fbd63870e4d98f084619e565db7c570121880a6
JohnCook17 <jcook0017@gmail.com>
2020-06-06 16:48:04 +0000

    now using try except, to only process values that the filter contains

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -36,20 +36,23 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     if isinstance(padding, tuple):
         p1 = padding[0]
         p2 = padding[1]
-        new_array_h = int((images.shape[1] + (2 * p1) - kernel.shape[0] + 1) /
-                          stride[0])
-        new_array_w = int((images.shape[2] + (2 * p2) - kernel.shape[1] + 1) /
-                          stride[1])
+        new_array_h = int((images.shape[1] + (2 * p1) - kernel.shape[0]) /
+                          stride[0]) + 1
+        new_array_w = int((images.shape[2] + (2 * p2) - kernel.shape[1]) /
+                          stride[1]) + 1
         new_array = np.empty((i, new_array_h, new_array_w))
         images = np.pad(array=images, pad_width=((0, 0), (p1, p1), (p2, p2)),
                         mode="constant", constant_values=0)
     for i_index in range(0, new_array.shape[1]):
         for j_index in range(0, new_array.shape[2]):
-            n = images[:, i_start: i_end, j_start: j_end]
-            new_pixel = np.sum((n * kernel), axis=(1, 2))
-            new_array[:, i_index, j_index] = new_pixel
-            j_start += stride[0]
-            j_end += stride[0]
+            try:
+                n = images[:, i_start: i_end, j_start: j_end]
+                new_pixel = np.sum((n * kernel), axis=(1, 2))
+                new_array[:, i_index, j_index] = new_pixel
+                j_start += stride[0]
+                j_end += stride[0]
+            except ValueError:
+                pass
         j_start = 0
         j_end = kernel.shape[1]
         i_start += stride[1]
