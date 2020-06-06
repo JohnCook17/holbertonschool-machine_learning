f356a1d8797daa58db2577ae72a8c5b76884b318
JohnCook17 <jcook0017@gmail.com>
2020-06-06 03:37:59 +0000

    changed condition

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -22,7 +22,7 @@ def convolve_grayscale_same(images, kernel):
     new_array_h = (images.shape[1] + 2 * p1 - kernel.shape[0] + 1)
     new_array_w = (images.shape[2] + 2 * p2 - kernel.shape[1] + 1)
     new_array = np.empty((i, new_array_h, new_array_w))
-    if new_array.shape == images.shape:
+    if images[0].shape[0] % kernel.shape[0] != 0 or images[0].shape[1] % kernel.shape[1]:
         images = np.pad(array=images, pad_width=((0, 0), (p1, p1), (p2, p2)),
                         mode="constant", constant_values=0)
     for i_index in range(0, new_array.shape[1]):
