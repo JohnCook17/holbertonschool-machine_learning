8bfc87ca8bc30153e3157153c9ada3aa4dcee2fb
JohnCook17 <jcook0017@gmail.com>
2020-06-11 17:37:17 +0000

    now uses stride

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -15,22 +15,23 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     ker_h = W.shape[0]
     ker_w = W.shape[1]
     if isinstance(padding, tuple):
-        p1, p2 = padding
+        ph, pw = padding
     elif padding == "same":
-        p1 = int(np.ceil((img_h - 1) * stride[0] + ker_h - img_h) / 2)
-        p2 = int(np.ceil((img_w - 1) * stride[1] + ker_w - img_w) / 2)
+        ph = int(np.ceil((img_h - 1) * stride[0] + ker_h - img_h) / 2)
+        pw = int(np.ceil((img_w - 1) * stride[1] + ker_w - img_w) / 2)
     elif padding == "valid":
-        p1, p2 = 0, 0
-    img_h_out = int((img_h + 2 * p1 - ker_h) / stride[0]) + 1
-    img_w_out = int((img_w + 2 * p2 - ker_w) / stride[1]) + 1
+        ph, pw = 0, 0
+    img_h_out = int((img_h + 2 * ph - ker_h) / stride[0]) + 1
+    img_w_out = int((img_w + 2 * pw - ker_w) / stride[1]) + 1
     out_shape = img_n, img_h_out, img_w_out, img_c_out
     r = np.zeros(shape=out_shape)
-    np.pad(r, pad_width=(p1, p2), mode="constant", constant_values=0)
+    np.pad(r, pad_width=(ph, pw), mode="constant", constant_values=0)
 
     for h in range(0, img_h_out):
         for w in range(0, img_w_out):
             for channel in range(0, img_c_out):
-                n = A_prev[:, h: h + ker_h, w: w + ker_w, :]
+                n = A_prev[:, h * stride[0]: (h + ker_h) * stride[0], w *
+                           stride[1]: (w + ker_w) * stride[1], :]
                 r[:, h, w, channel] = (np.sum((n * W[:, :, :, channel]),
                                               axis=(1, 2, 3)))
     return activation(r + b)
