a17e9388b77c0f6e9efb0440b957aa370c05bbb0
JohnCook17 <jcook0017@gmail.com>
2020-06-17 20:35:19 +0000

    maxpoolING instead of maxpool

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -10,42 +10,35 @@ def inception_block(A_prev, filters):
                                kernel_size=(1, 1),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu",
-                               name="conv2d_1")(A_prev)
+                               activation="relu")(A_prev)
     conv2d_3 = K.layers.Conv2D(filters=filters[3],
                                kernel_size=(1, 1),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu",
-                               name="conv2d_3")(A_prev)
-    max_pooling2d = K.layers.MaxPool2D(pool_size=(3, 3),
-                                       strides=(1, 1),
-                                       padding="same",
-                                       name="max_pooling2d")(A_prev)
+                               activation="relu")(A_prev)
+    max_pooling2d = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                          strides=(1, 1),
+                                          padding="same")(A_prev)
     conv2d = K.layers.Conv2D(filters=filters[0],
                              kernel_size=(1, 1),
                              strides=(1, 1),
                              padding="same",
-                             activation="relu",
-                             name="conv2d")(A_prev)
+                             activation="relu")(A_prev)
     conv2d_2 = K.layers.Conv2D(filters=filters[2],
                                kernel_size=(3, 3),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu",
-                               name="conv2d_2")(conv2d_1)
+                               activation="relu")(conv2d_1)
     conv2d_4 = K.layers.Conv2D(filters=filters[4],
                                kernel_size=(5, 5),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu",
-                               name="conv2d_4")(conv2d_3)
+                               activation="relu")(conv2d_3)
     conv2d_5 = K.layers.Conv2D(filters=filters[5],
                                kernel_size=(1, 1),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu",
-                               name="conv2d_5")(max_pooling2d)
+                               activation="relu")(max_pooling2d)
     concatenate = K.layers.concatenate([conv2d, conv2d_2, conv2d_4, conv2d_5],
                                        axis=-1)
     return concatenate
