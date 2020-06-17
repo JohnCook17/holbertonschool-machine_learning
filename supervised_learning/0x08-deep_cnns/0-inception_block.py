54da6e7fcf2103aa265b885c4737de12b81287ea
JohnCook17 <jcook0017@gmail.com>
2020-06-17 20:39:24 +0000

    he_normal kernel init

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -6,16 +6,19 @@ import tensorflow.keras as K
 def inception_block(A_prev, filters):
     """A_prev is the previous layer and filters is the number of filters to use
     """
+    initializer = K.initializers.he_normal()
     conv2d_1 = K.layers.Conv2D(filters=filters[1],
                                kernel_size=(1, 1),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu")(A_prev)
+                               activation="relu",
+                               kernel_initializer=initializer)(A_prev)
     conv2d_3 = K.layers.Conv2D(filters=filters[3],
                                kernel_size=(1, 1),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu")(A_prev)
+                               activation="relu",
+                               kernel_initializer=initializer)(A_prev)
     max_pooling2d = K.layers.MaxPooling2D(pool_size=(3, 3),
                                           strides=(1, 1),
                                           padding="same")(A_prev)
@@ -23,12 +26,14 @@ def inception_block(A_prev, filters):
                              kernel_size=(1, 1),
                              strides=(1, 1),
                              padding="same",
-                             activation="relu")(A_prev)
+                             activation="relu",
+                             kernel_initializer=initializer)(A_prev)
     conv2d_2 = K.layers.Conv2D(filters=filters[2],
                                kernel_size=(3, 3),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu")(conv2d_1)
+                               activation="relu",
+                               kernel_initializer=initializer)(conv2d_1)
     conv2d_4 = K.layers.Conv2D(filters=filters[4],
                                kernel_size=(5, 5),
                                strides=(1, 1),
@@ -38,7 +43,8 @@ def inception_block(A_prev, filters):
                                kernel_size=(1, 1),
                                strides=(1, 1),
                                padding="same",
-                               activation="relu")(max_pooling2d)
+                               activation="relu",
+                               kernel_initializer=initializer)(max_pooling2d)
     concatenate = K.layers.concatenate([conv2d, conv2d_2, conv2d_4, conv2d_5],
                                        axis=-1)
     return concatenate
