f8a7c2388205051dbd56faadb7bc57011c731cd5
JohnCook17 <jcook0017@gmail.com>
2020-06-17 21:02:31 +0000

    order matters

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -7,39 +7,39 @@ def inception_block(A_prev, filters):
     """A_prev is the previous layer and filters is the number of filters to use
     """
     initializer = K.initializers.he_normal()
-    conv2d_1 = K.layers.Conv2D(filters=filters[1],
-                               kernel_size=(1, 1),
-                               strides=(1, 1),
-                               padding="same",
-                               activation="relu",
-                               kernel_initializer=initializer)(A_prev)
-    conv2d_3 = K.layers.Conv2D(filters=filters[3],
-                               kernel_size=(1, 1),
-                               strides=(1, 1),
-                               padding="same",
-                               activation="relu",
-                               kernel_initializer=initializer)(A_prev)
-    max_pooling2d = K.layers.MaxPooling2D(pool_size=(3, 3),
-                                          strides=(1, 1),
-                                          padding="same")(A_prev)
     conv2d = K.layers.Conv2D(filters=filters[0],
                              kernel_size=(1, 1),
                              strides=(1, 1),
                              padding="same",
                              activation="relu",
                              kernel_initializer=initializer)(A_prev)
+    conv2d_1 = K.layers.Conv2D(filters=filters[1],
+                               kernel_size=(1, 1),
+                               strides=(1, 1),
+                               padding="same",
+                               activation="relu",
+                               kernel_initializer=initializer)(A_prev)
     conv2d_2 = K.layers.Conv2D(filters=filters[2],
                                kernel_size=(3, 3),
                                strides=(1, 1),
                                padding="same",
                                activation="relu",
                                kernel_initializer=initializer)(conv2d_1)
+    conv2d_3 = K.layers.Conv2D(filters=filters[3],
+                               kernel_size=(1, 1),
+                               strides=(1, 1),
+                               padding="same",
+                               activation="relu",
+                               kernel_initializer=initializer)(A_prev)
     conv2d_4 = K.layers.Conv2D(filters=filters[4],
                                kernel_size=(5, 5),
                                strides=(1, 1),
                                padding="same",
                                activation="relu",
                                kernel_initializer=initializer)(conv2d_3)
+    max_pooling2d = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                          strides=(1, 1),
+                                          padding="same")(A_prev)
     conv2d_5 = K.layers.Conv2D(filters=filters[5],
                                kernel_size=(1, 1),
                                strides=(1, 1),
