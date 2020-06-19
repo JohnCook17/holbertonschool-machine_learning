299fa795e5e5db33d37061c09a0bd26d82dd54b9
JohnCook17 <jcook0017@gmail.com>
2020-06-19 00:46:48 +0000

    7-densenet121.py

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-""""""
+"""densenet121 in keras"""
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
 
 def densenet121(growth_rate=32, compression=1.0):
-    """"""
+    """growth rate is how large the layer kernels are, compression is
+    how much they shrink during transition layers"""
     initializer = K.initializers.he_normal()
     inputs = K.Input(shape=(224, 224, 3))
     batch_normalization = K.layers.BatchNormalization()(inputs)
