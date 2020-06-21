0d831be41207cab103ffd7be4cfd6461251edf2a
JohnCook17 <jcook0017@gmail.com>
2020-06-21 18:50:36 +0000

    reworked

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -5,18 +5,33 @@ import tensorflow.keras as K
 
 def lenet5(X):
     """Keras version of lenet5"""
-    model = K.Sequential()
-    model.inputs = X
-    model.add(K.layers.Conv2D(filters=6, kernel_size=(5, 5), activation="relu",
-                              padding="SAME"))
-    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(K.layers.Conv2D(filters=16, kernel_size=(5, 5),
-                              activation="relu", padding="VALID"))
-    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(K.layers.Flatten())
-    model.add(K.layers.Dense(units=120, activation="relu"))
-    model.add(K.layers.Dense(units=84, activation="relu"))
-    model.add(K.layers.Dense(units=10, activation="softmax"))
+    initializer = K.initializers.he_normal()
+    inputs = X
+    layer_0 = K.layers.Conv2D(filters=6,
+                              kernel_size=(5, 5),
+                              activation="relu",
+                              padding="SAME",
+                              kernel_initializer=initializer)(inputs)
+    layer_1 = K.layers.MaxPooling2D(pool_size=(2, 2),
+                                    strides=(2, 2))(layer_0)
+    layer_2 = K.layers.Conv2D(filters=16,
+                              kernel_size=(5, 5),
+                              activation="relu",
+                              padding="VALID",
+                              kernel_initializer=initializer)(layer_1)
+    layer_3 = K.layers.MaxPooling2D(pool_size=(2, 2),
+                                    strides=(2, 2))(layer_2)
+    layer_4 = K.layers.Flatten()(layer_3)
+    layer_5 = K.layers.Dense(units=120,
+                             activation="relu",
+                             kernel_initializer=initializer)(layer_4)
+    layer_6 = K.layers.Dense(units=84,
+                             activation="relu",
+                             kernel_initializer=initializer)(layer_5)
+    outputs = K.layers.Dense(units=10,
+                             activation="softmax",
+                             kernel_initializer=initializer)(layer_6)
+    model = K.Model(inputs=inputs, outputs=outputs)
     opt = K.optimizers.Adam()
     model.compile(optimizer=opt, loss="categorical_crossentropy",
                   metrics=["accuracy"])
