876177425779dc98d5604630f56abec79cd270f4
JohnCook17 <jcook0017@gmail.com>
2020-07-06 19:21:52 +0000

    numbers are all wrong

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -51,40 +51,26 @@ class Yolo:
         boxes = []
         box_confidence = []
         box_class_probs = []
-        # print(self.anchors.shape)
+        image_height = image_size[0]
+        image_width = image_size[1]
+        input_shape = self.model.layers[0].input_shape
         for b, output in enumerate(outputs):
             anchors = self.anchors[b]
-            # print(anchors.shape)
-            # anchor_number = output.shape[2]
-            # print(anchors)
             grid_height, grid_width = output.shape[:2]
-            # print(grid_height, grid_width)
-            image_height = image_size[0]
-            image_width = image_size[1]
-            # print(image_height, image_width)
-            # output = output.reshape((grid_height, grid_width, anchor_number, -1))
-            # output[..., :2] = 1 / (1 + np.exp(-output[..., :2]))
-            # output[..., 4:] = 1 / (1 + np.exp(-output[..., 4:]))
-            input_shape = self.model.layers[0].input_shape
-            print(input_shape[1], input_shape[2])
+            anchor_boxes = output.shape[2]
             ph, pw = anchors.shape
-            # class_conf = output[:, :, :, 4] sigmoid here, watch shape
-            print(output[..., :4].shape)
+            class_conf = 1 / (1 + np.exp(output[:, :, :, 4]))
+            class_conf = class_conf.reshape(grid_height, grid_width, anchor_boxes, 1)
             box = output[..., :4]
-            x = 1 / (1 + np.exp(box[..., 0])) + np.arange(grid_width).reshape((1, grid_width, 1)) / grid_width * image_width
-            y = 1 / (1 + np.exp(box[..., 1])) + np.arange(grid_height).reshape((grid_height, 1, 1)) / grid_height * image_height
-            w = pw * np.exp(box[..., 2]) / input_shape[1] * image_width
-            h = ph * np.exp(box[..., 3]) / input_shape[2] * image_height
-            print(x.shape)
-            print(w.shape)
-            # print(x - w / 2)
-            # classes = output[:][:][:][5:] sigmoid here
-            box = np.array([x - w / 2, y - h / 2, x + w / 2, y + h / 2]) # check shape
-            print(box.shape)
+            x = 1 / (1 + np.exp(box[..., 0])) + np.arange(grid_width).reshape((1, grid_width, 1)) / grid_width
+            y = 1 / (1 + np.exp(box[..., 1])) + np.arange(grid_height).reshape((grid_height, 1, 1)) / grid_height
+            w = pw * np.exp(box[..., 2]) / input_shape[1]
+            h = ph * np.exp(box[..., 3]) / input_shape[2]
+            classes = 1 / (1 + np.exp(output[:][:][:][5:]))
+            box = np.array([x - w / 2, y - h / 2, x + w / 2, y + h / 2])
             box = np.moveaxis(box, 0, -1)
             boxes.append(box)
-            print(box.shape)
-            # box_confidence.append(class_conf)
-            # box_class_probs.append(classes)
+            box_confidence.append(class_conf)
+            box_class_probs.append(classes)
 
-        return boxes, None, None
+        return boxes, box_confidence, box_class_probs
