3b5071ca07d3219ced5e3902509b1777c2df9e07
JohnCook17 <jcook0017@gmail.com>
2020-07-23 23:44:16 +0000

    added a missing if

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -6,6 +6,8 @@ import numpy as np
 def definiteness(matrix):
     """calculates Positive definite, Positive semi-definite,
     Negative semi-definite, Negative definite, or Indefinite"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
     try:
         if np.where(np.linalg.eigvalsh(matrix) > 0,
                     True, False).all() is np.bool_(True):
