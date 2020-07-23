a80afc18b1fa730b0d4b16269b718f9760a1dfca
JohnCook17 <jcook0017@gmail.com>
2020-07-23 23:46:12 +0000

    added a size check

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -8,6 +8,8 @@ def definiteness(matrix):
     Negative semi-definite, Negative definite, or Indefinite"""
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
+    if matrix.shape[0] != matrix.shape[1]:
+        return None
     try:
         if np.where(np.linalg.eigvalsh(matrix) > 0,
                     True, False).all() is np.bool_(True):
