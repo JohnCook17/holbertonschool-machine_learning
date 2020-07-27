af7e0208e3b898f67760bb477acd4d2a29290da9
JohnCook17 <jcook0017@gmail.com>
2020-07-27 21:49:14 +0000

    docs

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""Mean and Covariance of a matrix"""
 import numpy as np
 
 
 def mean_cov(X):
-    """"""
+    """Returns the mean and covariance"""
     if not isinstance(X, np.ndarray) and len(X.shape) == 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""Correlation matrix"""
 import numpy as np
 
 
 def correlation(C):
-    """"""
+    """Returns the correlation matrix of a square matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) > 2 or C.shape[0] != C.shape[1]:
