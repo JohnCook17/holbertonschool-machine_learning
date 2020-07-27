540737d399c7ddd2ab7d15632e60b71936548071
JohnCook17 <jcook0017@gmail.com>
2020-07-27 23:10:10 +0000

    now considering that C is a covariance matrix

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -4,9 +4,14 @@ import numpy as np
 
 
 def correlation(C):
-    """Returns the correlation matrix of a square matrix"""
+    """Returns the correlation matrix of a square matrix,
+    that is a covariance matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) > 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
-    return np.corrcoef(C)
+    D = np.sqrt(np.diag(C))
+    outer = np.outer(D, D)
+    cor = C / outer
+    cor[C == 0] = 0
+    return cor
