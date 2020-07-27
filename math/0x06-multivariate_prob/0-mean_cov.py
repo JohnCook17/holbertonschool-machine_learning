89c59d0e177d74f24d8018b2e9fd6b31feda3896
JohnCook17 <jcook0017@gmail.com>
2020-07-27 22:40:22 +0000

    now not using np.cov

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -3,10 +3,22 @@
 import numpy as np
 
 
+def cov(x, y):
+    """covariance"""
+    xbar, ybar= x.mean(), y.mean()
+    return np.sum((x - xbar) * (y - ybar)) / (len(x) - 1)
+
+
+def cov_mat(X):
+    """"""
+    d, n = X.shape
+    return np.array([[cov(X[i], X[j]) for j in range(d)] for i in range(d)])
+
+
 def mean_cov(X):
     """Returns the mean and covariance"""
     if not isinstance(X, np.ndarray) and len(X.shape) == 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
-    return np.mean(X, axis=0, keepdims=True), np.cov(X, rowvar=False)
+    return np.mean(X, axis=0, keepdims=True), cov_mat(X.T)
