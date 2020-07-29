5a8a3fd5ab757ea2eedd186634acd4617a7d424b
JohnCook17 <jcook0017@gmail.com>
2020-07-29 19:26:36 +0000

    task 1

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""uses svd to get pca"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """finds the w of pca"""
+    u, s, vh = np.linalg.svd(X)
+    w = vh.T
+    return w[0:, 0:ndim]
