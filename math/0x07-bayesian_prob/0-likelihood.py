6fb8e240e1384cb74cf7ab3fdf162ed80288eede
JohnCook17 <jcook0017@gmail.com>
2020-07-31 20:15:18 +0000

    task 0 complete, init task 1

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""""""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """P(A | B) = P(B | A) * P(A) / P(B)"""
+    if n < 1:
+        raise ValueError("n must be a positive integer")
+    if x < 0:
+        raise ValueError("x must be an integer that is"
+                         " greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray):
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if np.all(P) < 0 or np.all(P) > 1:
+        raise ValueError("All values in P must be in the range [0, 1]")
+    fac = np.math.factorial
+    return (fac(n) / (fac(x) * fac(n - x))) * (P ** x) * ((1 - P) ** (n - x))
---
+++ math/0x07-bayesian_prob/README.md
@@ -0,0 +1,3 @@
+# 0x07. Bayesian Probability
+
+## Basic Bayesian probability task
