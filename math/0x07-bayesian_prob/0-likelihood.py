10ef4f98610bcb9bbd189f8caff492832a6cf393
JohnCook17 <jcook0017@gmail.com>
2020-07-31 21:01:12 +0000

    might have fixed conditions

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -7,8 +7,9 @@ def likelihood(x, n, P):
     """P(A | B) = P(B | A) * P(A) / P(B)"""
     if not isinstance(n, int) or n < 1:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or not x >= 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is"
+                         " greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
