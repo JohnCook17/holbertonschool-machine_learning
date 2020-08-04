cb0608933e16e2a9d2997416879560d95c1822fe
JohnCook17 <jcook0017@gmail.com>
2020-08-04 02:08:53 +0000

    docs

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""""""
+"""initializes K-means centroid"""
 import numpy as np
 
 
 def initialize(X, k):
-    """"""
+    """takes an array X of shape (n, d) where n is the number of data points
+    and d is the dimensions of each dp, and K the number of clusters"""
     try:
         if k < 1 or X.shape[1] < 1:
             return None
