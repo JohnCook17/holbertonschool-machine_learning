0832b44a1032fb1e5459221ed6b76d7c397748ff
JohnCook17 <jcook0017@gmail.com>
2020-08-06 23:05:39 +0000

    np.where might need some work

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -18,4 +18,4 @@ def pdf(X, m, S):
     X_t = ((np.linalg.inv(S)) @ Xm.T).T
     second_term = np.exp(-.5 * np.sum(Xm * X_t, axis=1))
     answer = first_term * second_term.T
-    return np.where(answer < 1e-300, 1e-300, answer)
+    return np.where(answer <= 0, 1e-300, answer)
