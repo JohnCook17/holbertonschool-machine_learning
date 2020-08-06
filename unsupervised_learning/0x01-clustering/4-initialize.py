ba306551e80b720779917aab1d51c706f2d4be92
JohnCook17 <jcook0017@gmail.com>
2020-08-06 20:01:55 +0000

    cond now

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -6,8 +6,13 @@ kmeans = __import__('1-kmeans').kmeans
 
 def initialize(X, k):
     """"""
-    n, d = X.shape
-    pi = np.ones(k) / k
-    m = kmeans(X, k)[0]
-    S = np.repeat(np.identity(d)[None, :], k, axis=0)
-    return pi, m, S
+    try:
+        if k < 1:
+            return None, None, None
+        n, d = X.shape
+        pi = np.ones(k) / k
+        m = kmeans(X, k)[0]
+        S = np.repeat(np.identity(d)[None, :], k, axis=0)
+        return pi, m, S
+    except Exception as e:
+        return None, None, None
