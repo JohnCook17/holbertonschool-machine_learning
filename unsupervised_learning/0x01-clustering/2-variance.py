8c427921c3873ccbad6443b81d36e7314cf3bd17
JohnCook17 <jcook0017@gmail.com>
2020-08-06 15:35:06 +0000

    if K

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -6,6 +6,8 @@ import numpy as np
 def variance(X, C):
     """Cluster variance, to find how many clusters to use"""
     try:
+        if C.shape[0] <= 1:
+            return None
         diff = X[:, :, np.newaxis] - C[:, :, np.newaxis].T
         norm = np.linalg.norm(diff, axis=1)
         mini = np.min(norm, axis=1)
