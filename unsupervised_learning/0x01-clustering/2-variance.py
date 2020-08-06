2ba92ba6d68d84ec478fe923b4ff9f724daf11cf
JohnCook17 <jcook0017@gmail.com>
2020-08-06 23:14:25 +0000

    added cond

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -6,6 +6,12 @@ import numpy as np
 def variance(X, C):
     """Cluster variance, to find how many clusters to use"""
     try:
+        if not isinstance(C, np.ndarray):
+            return None
+        if len(C.shape) != 2:
+            return None
+        if C.shape[1] != X.shape[1]:
+            return None
         if C.shape[0] < 1:
             return None
         diff = X[:, :, np.newaxis] - C[:, :, np.newaxis].T
