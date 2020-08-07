96ed5c2780e94b1caffdb09a9f52a29ab8825e64
JohnCook17 <jcook0017@gmail.com>
2020-08-07 01:57:34 +0000

    added more cond

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -7,22 +7,25 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """finds the optimum k"""
-    try:
-        if not isinstance(kmin, int) or not isinstance(kmax, int):
-            return None, None
-        if kmin < 1:
-            return None, None
-        if kmin >= kmax:
-            return None, None
-        res = []
-        var_list = []
-        for k in range(kmin, kmax + 1):
-            C, clss = kmeans(X, k, iterations)
-            res.append((C, clss))
-            var = variance(X, C)
-            if k == kmin:
-                var_first = var
-            var_list.append(var_first - var)
-        return res, var_list
-    except Exception as e:
+    if kmax is None:
+        kmax = iterations
+    if kmin is None:
+        kmin = 1
+    if not isinstance(kmin, int) or not isinstance(kmax, int):
         return None, None
+    if kmin < 1:
+        return None, None
+    if kmin >= kmax:
+        return None, None
+    res = []
+    var_list = []
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        if isinstance(C, np.ndarray) and isinstance(clss, np.ndarray):
+            res.append((C, clss))
+        var = variance(X, C)
+        if k == kmin:
+            var_first = var
+        if isinstance(var, float):
+            var_list.append(var_first - var)
+    return res, var_list
