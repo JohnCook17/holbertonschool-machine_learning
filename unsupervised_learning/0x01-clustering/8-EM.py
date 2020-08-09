c093025d4070950c65ad381d0542ea357dfab3e7
JohnCook17 <jcook0017@gmail.com>
2020-08-09 18:46:08 +0000

    np.isclose works now, docs

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""""
+"""Expectation maximization"""
 import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
@@ -7,21 +7,22 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """"""
+    """performs expectation maximization oh the X data set, with k clusters"""
     i = 0
     log_likelihood = 0
     diff = 0
     pi, m, S = initialize(X, k)
-    while i < iterations or not np.isclose(diff, np.round(log_likelihood, 5),
-                                           rtol=tol):
+    while i < iterations:
         g, log_likelihood = expectation(X, pi, m, S)
-        if i % 10 == 0 and verbose is True:
+        if i % 10 == 0 and verbose:
             print("Log Likelihood after {} iterations: {}"
                   .format(i, np.round(log_likelihood, 5)))
-        diff = np.round(log_likelihood, 5)
         pi, m, S = maximization(X, g)
+        if np.isclose(diff, log_likelihood, atol=tol, rtol=0):
+            break
         i += 1
-    if verbose is True:
+        diff = log_likelihood
+    if verbose:
         print("Log Likelihood after {} iterations: {}"
               .format(i, np.round(log_likelihood, 5)))
     return pi, m, S, g, log_likelihood
