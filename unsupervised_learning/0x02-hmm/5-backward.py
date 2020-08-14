e46b6d13872f2ad880a5ba8d5e041b4d9de3bafb
JohnCook17 <jcook0017@gmail.com>
2020-08-14 17:50:43 +0000

    task 5

---
+++ unsupervised_learning/0x02-hmm/5-backward.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A numpy version of backward"""
+import numpy as np
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """Backward algorithm for HMM"""
+    N = Emission.shape[0]
+    T = Observation.shape[0]
+    B = np.zeros((N, T))
+    for s in range(N - 1, -1, -1):
+        B[s, T - 1] = 1
+    for t in range(T - 2, -1, -1):
+        for s in range(N):
+            B[s, t] = np.sum(Transition[s, :] * Emission[:, Observation[t + 1]]
+                             * B[:, t + 1])
+    P = np.sum(B[:, 0])
+    return P, B
