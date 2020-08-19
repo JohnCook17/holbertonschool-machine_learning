789eb1d012a5add78d4039e64c65626d7e5c9f94
JohnCook17 <jcook0017@gmail.com>
2020-08-19 20:42:04 +0000

    task 0-3 done, task 4 wip, 3 might need l instead of newaxis

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""""""
+"""Gaussian Process"""
 import numpy as np
 
 
 class GaussianProcess:
-    """"""
+    """The Guassian Process optimization class"""
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """"""
+        """init values for later calculations"""
         self.X = X_init
         self.Y = Y_init
         self.l = l
@@ -16,7 +16,7 @@ class GaussianProcess:
         self.K = self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
 
     def kernel(self, X1, X2):
-        """"""
+        """The kernel to use in later calculations"""
         sqdist = (np.sum(X1 ** 2, 1).reshape(-1, 1) +
                   np.sum(X2 ** 2, 1) - 2 * np.matmul(X1, X2.T))
         return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -22,3 +22,16 @@ class BayesianOptimization():
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples)[:, np.newaxis]
         self.xsi = xsi
         self.minimize = minimize
+
+    def acquisition(self):
+        """"""
+        mu, sigma = self.gp.predict(self.X_s)
+        mu_sample = self.gp.predict(self.gp.X)
+        # sigma = sigma.reshape(-1, 1)
+        mu_max = np.max(mu_sample)
+        with np.errstate(divide="warn"):
+            imp = mu - mu_max - self.xsi
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            ei[sigma == 0.0] = 0.0
+        return mu_max, ei
