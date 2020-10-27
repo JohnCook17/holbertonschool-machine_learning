2bf8891a9ff58361ca19d51fc787cb528e489e1d
JohnCook17 <jcook0017@gmail.com>
2020-10-27 02:53:47 +0000

    task 1

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""""""
+import numpy as np
+
+
+def correlation(C):
+    """"""
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    R = np.zeros((C.shape))
+    for i in range(C.shape[0]):
+        for j in range(C.shape[0]):
+            R[i][j] = C[i][j] / np.sqrt(C[i][i] * C[j][j])
+    
+    return R
