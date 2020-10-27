f1c766ae2d25f5066bfdfec20c9f2c29fe380e21
JohnCook17 <jcook0017@gmail.com>
2020-10-27 02:55:37 +0000

    docs

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""finds the correlation given C a covariance matrix"""
 import numpy as np
 
 
 def correlation(C):
-    """"""
+    """C is already a covariance matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
@@ -14,5 +14,5 @@ def correlation(C):
     for i in range(C.shape[0]):
         for j in range(C.shape[0]):
             R[i][j] = C[i][j] / np.sqrt(C[i][i] * C[j][j])
-    
+
     return R
