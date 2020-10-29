123f2d5c3de3e79701d33d0ee01cf8910397c1ba
JohnCook17 <jcook0017@gmail.com>
2020-10-29 00:55:57 +0000

    task 0

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""PCA of an array to reduce the number of features"""
 import numpy as np
 
 
 def pca(X, var=0.95):
-    """"""
+    """performs pca on a matrix"""
     W, V = np.linalg.eig(np.matmul(X.T, X))
     W_idx = W.argsort()[::-1]
     V = V[:, W_idx]
