c40f8b95519f08f48118eda28017dc76681816e5
JohnCook17 <jcook0017@gmail.com>
2020-10-29 02:44:47 +0000

    docs

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""Performs PCA on a matrix"""
 import numpy as np
 
 
 def pca(X, ndim):
-    """"""
+    """Returns T after performing PCA on a matrix"""
     X -= np.mean(X, axis=0)
     W, V = np.linalg.eig((np.cov(X.T)))
     # idx = W.argsort()[::-1]
