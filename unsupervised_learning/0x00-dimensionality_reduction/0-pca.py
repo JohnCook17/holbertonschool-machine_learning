8764a6f869ef011e54a80e949073a4abe81ce741
JohnCook17 <jcook0017@gmail.com>
2020-10-29 00:52:19 +0000

    task 0

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""""""
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """"""
+    W, V = np.linalg.eig(np.matmul(X.T, X))
+    W_idx = W.argsort()[::-1]
+    V = V[:, W_idx]
+    # print(V)
+    V_var = np.copy(V)
+    V_var *= 1 / np.abs(V_var).max()
+    # print(V_var)
+    V_idx = V[np.where(np.abs(V_var) >= var, True, False)]
+    # print(V_idx.shape)
+    V_idx = len(V_idx)
+    # print(V[:, :V_idx].shape)
+    return V[:, :V_idx] * -1
