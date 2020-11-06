1188f63dca19a39aefbc6a95fcadc2a47d31fe32
JohnCook17 <jcook0017@gmail.com>
2020-11-06 19:06:51 +0000

    task 11, docs for 10

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""""""
+"""k-means in sklearn"""
 import sklearn.cluster
 
 
 def kmeans(X, k):
-    """"""
+    """The k-means function form sklearn"""
     k_means = sklearn.cluster.KMeans(n_clusters=k).fit(X)
     return k_means.cluster_centers_, k_means.labels_
---
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Gaussian Mixture Model in sklearn"""
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """The gaussian mixture model"""
+    gauss_mix = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
+
+    pi = gauss_mix.predict_proba(X)
+    m = gauss_mix.means_
+    S = gauss_mix.covariances_
+    clss = gauss_mix.predict(X)
+    bic = gauss_mix.bic(X)
+
+    return pi, m, S, clss, bic
