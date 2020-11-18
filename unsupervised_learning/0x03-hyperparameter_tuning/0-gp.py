b9d0a80117e1cf9b8446b192dba50bddd4b46e7e
JohnCook17 <jcook0017@gmail.com>
2020-11-18 19:02:18 +0000

    docs and style

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""""""
+"""The Gaussian Process used in optimization"""
 import numpy as np
 
 
 class GaussianProcess():
-    """"""
+    """The Gaussian Process Class"""
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """"""
+        """init of variables used in the gp"""
         self.X = X_init
         self.Y = Y_init
         self.l = l
@@ -14,5 +14,6 @@ class GaussianProcess():
         self.K = self.kernel(self.X, self.Y)
 
     def kernel(self, X1, X2):
-        """"""
-        return (self.sigma_f ** 2) * np.exp((-(1 / (2 * (self.l ** 2))) * ((X1 - X2).T * (X1 - X2))))
+        """The Radial Basis Function to get the kernel"""
+        return ((self.sigma_f ** 2) *
+                np.exp(-(1 / (2 * (self.l ** 2))) * ((X1 - X2).T * (X1 - X2))))
