c99980b30a6de103128fe26bb68c19a61ff7bbcc
JohnCook17 <jcook0017@gmail.com>
2020-11-19 23:37:11 +0000

    task 5 progress

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/5-bayes_opt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Bayesian Optimization"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization():
+    """The Bayesian Optimization class"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """init of Bayesian Optimization"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = (np.linspace(start=bounds[0], stop=bounds[1],
+                                num=ac_samples)[:, np.newaxis])
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """Acquires X_next, and Expectation improvement"""
+        mu, sigma = self.gp.predict(self.X_s)
+
+        if self.minimize is True:
+            mu_sample_opt = np.min(self.gp.Y)
+        else:
+            mu_sample_opt = np.max(self.gp.Y)
+
+        with np.errstate(divide="warn"):
+            imp = mu_sample_opt - mu - self.xsi
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            ei[ei == 0.0] = 0.0
+        X = self.X_s[np.argmax(ei, axis=0)]
+        return X, ei
+
+    def optimize(self, iterations=100):
+        """"""
+        for _ in range(iterations):
+            X, ei = self.acquisition()
+            self.gp.update(X, self.f(X))
+        return X, self.f(X)
