736ff4707920e094c815989626db732c7e673b29
JohnCook17 <jcook0017@gmail.com>
2020-11-19 00:31:57 +0000

    task 2

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""The Gaussian Process used in optimization"""
+import numpy as np
+
+
+class GaussianProcess():
+    """The Gaussian Process Class"""
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """init of variables used in the gp"""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """The Radial Basis Function to get the kernel"""
+        sqdist = (np.sum(X1**2, 1).reshape(-1, 1) +
+                  np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T))
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
+
+    def predict(self, X_s):
+        """predicts the mu and sigma values"""
+        K = self.kernel(self.X, self.X)
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(K)
+
+        mu_s = K_s.T.dot(K_inv).dot(self.Y)
+
+        sigma = K_ss - K_s.T.dot(K_inv).dot(K_s)
+
+        return mu_s.flatten(), np.diag(sigma)
+
+    def update(self, X_new, Y_new):
+        """appends X_new to self.X and Y_new to self.Y and then updates the
+        kernel"""
+        self.X = np.vstack((self.X, X_new[np.newaxis]))
+        self.Y = np.vstack((self.Y, Y_new[np.newaxis]))
+        self.K = self.kernel(self.X, self.X)
