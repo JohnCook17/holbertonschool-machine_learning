50c6cf537af69bf604135fbf997c92a97032eece
JohnCook17 <jcook0017@gmail.com>
2020-11-28 20:18:27 +0000

    now handles invalid k and returns centroids in desired order.

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -5,10 +5,12 @@ import numpy as np
 
 def initialize(X, k):
     """Initializes for K means given the data X"""
+    if k < 1:
+        return None
     try:
         d = X.shape[1]
-        data_min = np.min(X, axis=0)
-        data_max = np.max(X, axis=0)
+        data_min = np.min(X, axis=1)
+        data_max = np.max(X, axis=1)
         return np.random.uniform(low=data_min, high=data_max, size=(k, d))
     except Exception as e:
         return None
