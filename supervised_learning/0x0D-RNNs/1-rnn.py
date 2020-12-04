ebea129f1174843dbd87ee6aabf5cb9122deebdf
JohnCook17 <jcook0017@gmail.com>
2020-12-04 22:48:22 +0000

    matches local output

--- supervised_learning/0x0D-RNNs/1-rnn.py
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -5,11 +5,13 @@ import numpy as np
 
 def rnn(rnn_cell, X, h_0):
     """"""
-    H = np.zeros(shape=(X.shape[0] + 1, h_0.shape[0], h_0.shape[1]))
+    H = []
     Y = []
+    H.append(h_0)
+    h = h_0
     for time_step in range(X.shape[0]):
-        h, y = rnn_cell.forward(h_0, X[time_step])
-        H[time_step + 1:, :, :] = h
+        h, y = rnn_cell.forward(h, X[time_step])
+        H.append(h)
         Y.append(y)
 
     return np.asarray(H), np.asarray(Y)
