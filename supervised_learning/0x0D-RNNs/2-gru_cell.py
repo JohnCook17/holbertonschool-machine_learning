fixed softmax, working on 4, 7, 8

supervised_learning/0x0D-RNNs/2-gru_cell.py
@@ -22,15 +22,17 @@ class GRUCell():
 
     def softmax(self, x):
         """"""
-        e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum(axis=0)
+        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return e_x / e_x.sum(axis=1, keepdims=True)
 
     def forward(self, h_prev, x_t):
         """"""
         concat = np.concatenate((h_prev, x_t), axis=1)
+        
         z_t = self.sigmoid(np.matmul(concat, self.Wz) + self.bz)
         r_t = self.sigmoid(np.matmul(concat, self.Wr) + self.br)
-        h_hat_t = np.tanh(np.matmul(concat, self.Wh) + (r_t * h_prev) + self.bh)
+        intermidiate = np.concatenate((r_t * h_prev, x_t), axis=1)
+        h_hat_t = np.tanh(np.matmul(intermidiate, self.Wh) + self.bh)
         h_t = (1 - z_t) * h_prev + z_t * h_hat_t
         y = self.softmax(np.matmul(h_t, self.Wy) + self.by)
 

supervised_learning/0x0D-RNNs/3-lstm_cell.py
@@ -24,8 +24,8 @@ class LSTMCell():
 
     def softmax(self, x):
         """"""
-        e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum(axis=0)
+        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return e_x / e_x.sum(axis=1, keepdims=True)
 
     def forward(self, h_prev, c_prev, x_t):
         """"""

supervised_learning/0x0D-RNNs/4-deep_rnn.py
@@ -5,12 +5,21 @@ import numpy as np
 
 def deep_rnn(rnn_cells, X, h_0):
     """"""
-    H = np.zeros(shape=(X.shape[0] + 1, h_0.shape[0], h_0.shape[1], h_0.shape[2]))
+    H = h_0
+    H_o = np.zeros((X.shape[0] + 1, h_0.shape[0], h_0.shape[1], h_0.shape[2]))
+    print(H_o.shape)
     Y = []
     for time_step in range(X.shape[0]):
+        h = None
         for depth, layer in enumerate(rnn_cells):
-            h, y = layer.forward(h_0[depth], X[time_step])
-            H[time_step + 1:, depth, :, :] = h
+            # print(time_step, depth)
+            if h is None:
+                h, y = layer.forward(H[depth], X[time_step])
+            else:
+                h, y = layer.forward(H[depth], h)
+            # print(H.shape, h.shape)
+            H[depth, :, :] = h
+        H_o[time_step + 1, :, :, :] = H
+        H = np.zeros(h_0.shape)
         Y.append(y)
-
-    return np.asarray(H), np.asarray(Y)
+    return H_o, np.asarray(Y)