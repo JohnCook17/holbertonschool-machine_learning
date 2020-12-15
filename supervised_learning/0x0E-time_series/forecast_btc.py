810ce5d46c07f4c9b7906a4f504ad1f0a0bedc24
JohnCook17 <jcook0017@gmail.com>
2020-12-15 02:37:35 +0000

    forecast_btc.py

---
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""""""
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+def forcast():
+    tf.enable_eager_execution()
+
+    train_path = "data/bitstampUSD_1-min_data_2012-01-01_to_2020-04-22_preprocessed.csv"
+
+    train_array = np.genfromtxt(train_path, delimiter=",", skip_header=True)
+    values = train_array[:, 0].copy()
+    targets = train_array[:, 1].copy()
+
+    values = tf.data.Dataset.from_tensor_slices([values])
+    targets = tf.data.Dataset.from_tensor_slices(targets)
+
+    train_dataset = tf.data.Dataset.zip((values, targets))
+
+    model = keras.models.Sequential([keras.layers.LSTM(units=(24, 1), return_sequences=True)])
+
+    model.compile(loss="mean_squared_error", optimizer=tf.train.AdamOptimizer())
+
+    print(train_dataset, "\n")
+    for element in train_dataset:
+        print(element)
+
+    history = model.fit(x=train_dataset, steps_per_epoch=1, validation_split=False, epochs=1)
+
+    return history
+
+history = forcast()
+print(history)
