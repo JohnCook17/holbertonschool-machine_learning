68eafbe1dc837dae8507f5e4a88ae0977793d724
JohnCook17 <jcook0017@gmail.com>
2020-12-17 05:14:37 +0000

    pycodestyle, need to shorten paths

--- supervised_learning/0x0E-time_series/forecast_btc.py
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -2,7 +2,6 @@
 """"""
 import numpy as np
 import tensorflow as tf
-# import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 from os import path
 
@@ -16,15 +15,29 @@ def forcast():
     validate_path = "data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09_preprocessed.csv"
     validate_target_path = "data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09targets_preprocessed.csv"
 
-    values = np.genfromtxt(train_path, delimiter=",", skip_header=True, dtype="float32")
-    targets = np.genfromtxt(train_target_path, delimiter=",", skip_header=True, dtype="float32")
-    
-    validate_vs = np.genfromtxt(validate_path, delimiter=",", skip_header=True, dtype="float32")
-    validate_ts = np.genfromtxt(validate_target_path, delimiter=",", skip_header=True, dtype="float32")
+    values = np.genfromtxt(train_path,
+                           delimiter=",",
+                           skip_header=True,
+                           dtype="float32")
+
+    targets = np.genfromtxt(train_target_path,
+                            delimiter=",",
+                            skip_header=True,
+                            dtype="float32")
+
+    validate_vs = np.genfromtxt(validate_path,
+                                delimiter=",",
+                                skip_header=True,
+                                dtype="float32")
+
+    validate_ts = np.genfromtxt(validate_target_path,
+                                delimiter=",",
+                                skip_header=True,
+                                dtype="float32")
 
     values = values.reshape(1, 24, 1)
     targets = targets[np.newaxis].reshape(1, 1, 1)
-    
+
     validate_vs = validate_vs.reshape(1, 24, 1)
     validate_ts = validate_ts.reshape(1, 1, 1)
 
@@ -40,15 +53,25 @@ def forcast():
 
     train_dataset = tf.data.Dataset.zip((values, targets))
 
-    validation_dataset = tf.data.Dataset.zip((validate_values, validate_target))
+    validation_dataset = tf.data.Dataset.zip((validate_values,
+                                              validate_target))
 
-    model = tf.keras.models.Sequential([tf.keras.layers.InputLayer(input_shape=(1, 24, 1)),
-                                     tf.keras.layers.LSTM(units=1, return_sequences=True, activation="relu"),
-                                     tf.keras.layers.Dense(units=1)])
+    model = (tf.keras.models
+             .Sequential([tf.keras.layers.InputLayer(input_shape=(1, 24, 1)),
+                          tf.keras.layers.LSTM(units=1,
+                                               return_sequences=True,
+                                               activation="relu"),
+                          tf.keras.layers.Dense(units=1)]))
 
-    model.compile(loss="mean_squared_error", optimizer=tf.train.AdamOptimizer(learning_rate=1))
+    model.compile(loss="mean_squared_error",
+                  optimizer=tf.train.AdamOptimizer(learning_rate=1))
 
-    history = model.fit(train_dataset, steps_per_epoch=1, batch_size=1, validation_split=False, epochs=4500, validation_data=validation_dataset)
+    history = model.fit(train_dataset,
+                        steps_per_epoch=1,
+                        batch_size=1,
+                        validation_split=False,
+                        epochs=4500,
+                        validation_data=validation_dataset)
 
     model.save("saved_model/lstm")
 
@@ -59,14 +82,17 @@ if __name__ == "__main__":
 
     if path.exists("saved_model/lstm"):
         model = tf.keras.models.load_model("saved_model/lstm")
-        model.compile(loss="mean_squared_error", optimizer=tf.train.AdamOptimizer(learning_rate=1))
+        model.compile(loss="mean_squared_error",
+                      optimizer=tf.train.AdamOptimizer(learning_rate=1))
     else:
         model = forcast()
 
     history = model.history
 
     prediction_path = "data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09_prediction.csv"
-    prediction_data = np.genfromtxt(prediction_path, delimiter=",", skip_header=True)
+    prediction_data = np.genfromtxt(prediction_path,
+                                    delimiter=",",
+                                    skip_header=True)
 
     prediction_data = prediction_data[np.newaxis].reshape(24, 1, 1, 1)
     print(prediction_data.shape)
@@ -77,5 +103,5 @@ if __name__ == "__main__":
     print(prediction_data)
 
     my_prediction = model.predict(prediction_data, steps=1)
-    
+
     print(my_prediction)
--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from os import path
 
+
 def preprocess(df_to_load):
     """"""
 
@@ -12,7 +13,7 @@ def preprocess(df_to_load):
 
     print(df.dtypes)
 
-    print("====================PROCESSING DATA====================")
+    print("====================PROCESSING DATA=================")
 
     print("Dropping NAN's")
     df = df.dropna()
@@ -25,7 +26,11 @@ def preprocess(df_to_load):
     print("======================")
 
     print("Removing unneeded values")
-    df = df.drop(labels=["Open", "High", "Low", "Volume_(BTC)", "Weighted_Price"], axis=1)
+    df = df.drop(labels=["Open",
+                         "High",
+                         "Low",
+                         "Volume_(BTC)",
+                         "Weighted_Price"], axis=1)
 
     print("======================")
     print(df.dtypes)
@@ -54,20 +59,24 @@ def preprocess(df_to_load):
     print("++++++++++++++++++++++++++++")
 
     # print("Normalizing data")
-    # df["Close"] = df["Close"] * df["Volume_(Currency)"] / df["Volume_(Currency)"].sum()
+    # df["Close"] = (df["Close"] *
+    #                df["Volume_(Currency)"] / df["Volume_(Currency)"].sum())
 
     print("getting Hours in day")
     my_data = []
     for i in range(1, 26):
         start_time = coin_base_date - np.timedelta64(i, "h")
         end_time = coin_base_date - np.timedelta64(i - 1, "h")
-        my_data.append(df[df.Timestamp.between(start_time, end_time)]["Close"].values[-1])
+        my_data.append(df[df.Timestamp.between(start_time,
+                                               end_time)]["Close"].values[-1])
 
     my_p_data = []
     for i in range(1, 25):
         p_start_time = coin_base_date - np.timedelta64(i, "h")
         p_end_time = coin_base_date - np.timedelta64(i - 1, "h")
-        my_p_data.append(p_df[p_df.Timestamp.between(p_start_time, p_end_time)]["Close"].values[-1])
+        my_p_data.append(p_df[p_df.Timestamp
+                              .between(p_start_time,
+                                       p_end_time)]["Close"].values[-1])
 
     print("making new dfs")
     new_df = pd.DataFrame(my_data, columns=["inputs"])
@@ -79,11 +88,7 @@ def preprocess(df_to_load):
     new_p_df.drop_duplicates(subset="inputs", keep="last")
     targets_df = pd.DataFrame()
     targets_df["targets"] = new_df["inputs"].tail(1)
-    # new_df["targets"] = ""
-    # new_df.ix[0, "targets"] = new_df["inputs"].values[-1]
-    # new_df.drop_duplicates(subset="Target", keep="first")
     new_df.drop(new_df.tail(1).index, inplace=True)
-    # new_df["Target"] = new_df["Hour"].shift(periods=-1,fill_value=new_df["Hour"].values[-1])
 
     print("======================")
     print(new_df.head(25))
@@ -93,11 +98,14 @@ def preprocess(df_to_load):
 
     new_df.to_pickle(df_to_load[:-4] + "_pickle")
     new_df.to_csv(df_to_load[:-4] + "_preprocessed.csv", index=False)
-    targets_df.to_csv(df_to_load[:-4] + "targets_preprocessed.csv", index=False)
+    targets_df.to_csv(df_to_load[:-4] + "targets_preprocessed.csv",
+                      index=False)
     new_p_df.to_csv(df_to_load[:-4] + "_prediction.csv", index=False)
+
 
 if __name__ == "__main__":
 
-    dfs = ["data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv", "data/bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv"]
+    dfs = ["data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv",
+           "data/bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv"]
     for data in dfs:
         preprocess(data)
