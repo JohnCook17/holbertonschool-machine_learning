be2d6a459be792cd2ad3405157133fe71bd0da6f
JohnCook17 <655@holbertonschool.com>
2020-12-24 05:38:41 +0000

    closer?

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -25,17 +25,17 @@ def count_ngram(unigram, ngram=1):
     return my_count
 
 
-def modified_precision(references, sentence, n):
+def modified_precision(ref, sentence, n):
     """"""
     counts = count_ngram(sentence, n)
     max_counts = {}
 
-    for ref in references:
-        ref_ngram = count_ngram(ref, n)
-        for ngram in ref_ngram.keys():
-            max_counts[ngram] = max(max_counts.get(ngram, 0), ref_ngram[ngram])
+    # for ref in references:
+    ref_ngram = count_ngram(ref, n)
+    for ngram in counts:
+        max_counts[ngram] = max(max_counts.get(ngram, 0), ref_ngram.get(ngram, 0))
     
-    clipped_counts = {ngram: min(count, max_counts.get(ngram, 0)) for ngram, count in counts.items()}
+    clipped_counts = {ngram: min(count, max_counts[ngram]) for ngram, count in counts.items()}
 
     numerator = sum(clipped_counts.values())
 
@@ -49,23 +49,30 @@ def modified_precision(references, sentence, n):
 def uni_bleu(references, sentence):
     """"""
     n = 1
-    p_num = {}
-    p_den = {}
+    weights = [1 / n for i in range(n)] # change this to 1 / n n times
+    p_num = 0
+    p_den = 0
+    p_num_total = {i: 0 for i in range(1, len(weights) + 1)}
+    p_den_total = {i: 0 for i in range(1, len(weights) + 1)}
     sen_lens = 0
     ref_lengths = 0
-    weights = [1 / n for i in range(n)] # change this to 1 / n n times
     print(weights)
     print("===========")
     for ref in references:
         for i in range(1, len(weights) + 1):
-            p_num[i], p_den[i] = modified_precision(references, sentence, n)
+            p_num, p_den = modified_precision(ref, sentence, n)
+            print(p_num, p_den)
+            p_num_total[i] += p_num
+            p_den_total[i] += p_den
             sen_len = len(sentence)
             sen_lens += sen_len
             ref_lengths += (np.abs(np.asarray([len(x) for x in references]) - len(sentence))).argmin()
 
     best = (np.abs(np.asarray([len(x) for x in references]) - len(sentence))).argmin()
 
-    p_n = [p_num[i] / p_den[i] for i in range(1, len(weights) + 1)]
+    print(p_num_total, p_den_total)
+
+    p_n = [p_num_total[i] / p_den_total[i] for i in range(1, len(weights) + 1)]
 
     # brevity penalty
     if len(sentence) > best:
@@ -74,6 +81,7 @@ def uni_bleu(references, sentence):
         bp = np.exp(1 - (ref_lengths / sen_lens))
     # end brevity penalty
 
+    print("p_n = ", p_n)
     s = (w_i * np.log(p_i) for w_i, p_i in zip(weights, p_n))
     print("bp = ", bp)
     s = np.exp(np.sum(s))
