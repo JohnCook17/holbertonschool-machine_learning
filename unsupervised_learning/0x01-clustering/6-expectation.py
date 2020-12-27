02d98e4577c4aac6f462368f20d106f76cddc1e2
JohnCook17 <655@holbertonschool.com>
2020-12-27 22:17:33 +0000

    6-expectation.py

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
-""""""
+"""E step of gmm"""
 import numpy as np
+pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    """"""
-    return None
+    """Performs the 'e' step of gmm"""
+    k = pi.shape[0]
+    numerator = []
+    for i in range(k):
+        numerator.append(pi[i] * pdf(X, m[i], S[i]))
+    numerator = np.asarray(numerator)
+
+    l = np.sum(np.log(np.sum(numerator, axis=0)))
+    
+    g = numerator / np.sum(numerator, axis=0)
+
+    return g, l
