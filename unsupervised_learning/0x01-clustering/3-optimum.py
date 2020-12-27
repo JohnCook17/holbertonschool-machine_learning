8a54c56b82f489995f280ee590dec198024f8c52
JohnCook17 <655@holbertonschool.com>
2020-12-27 01:59:55 +0000

    missing validation conditionals, will add next

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 """"""
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """"""
-    return None
+    res = []
+    d_vars = []
+    var = 0
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        var = variance(X, C)
+        if k == kmin:
+            new_var = var
+        res.append((C, clss))
+        if isinstance(var, float):
+            d_vars.append(new_var - var)
+    
+    return res, d_vars
+
