44dc16939ec22a13abc60c7dd5b225fffd27de8a
JohnCook17 <655@holbertonschool.com>
2020-12-27 22:29:14 +0000

    conditionals

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -6,14 +6,37 @@ pdf = __import__('5-pdf').pdf
 
 def expectation(X, pi, m, S):
     """Performs the 'e' step of gmm"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+
+    if pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]:
+        return None, None
+
+    d = X.shape[1]
     k = pi.shape[0]
+
+    if m.shape[0] != k or m.shape[1] != d:
+        return None, None
+
+    if S.shape[0] != K or S.shape[1] != d: or S.shape[2] != d:
+        return None, None
+
     numerator = []
     for i in range(k):
         numerator.append(pi[i] * pdf(X, m[i], S[i]))
     numerator = np.asarray(numerator)
 
-    l = np.sum(np.log(np.sum(numerator, axis=0)))
-    
+    likelyhood = np.sum(np.log(np.sum(numerator, axis=0)))
+
     g = numerator / np.sum(numerator, axis=0)
 
-    return g, l
+    return g, likelyhood
