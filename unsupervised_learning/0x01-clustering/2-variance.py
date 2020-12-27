d0ffd9cfc863afdaa7534b730a857666de6968e9
JohnCook17 <655@holbertonschool.com>
2020-12-27 01:03:16 +0000

    2-variance.py

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
-""""""
+"""Intra cluster variance"""
 import numpy as np
 
 
 def variance(X, C):
-    """""""
-    return None
+    """Finds the variance of X clusters with C centers"""
+    if not isinstance(C, np.ndarray):
+        return None
+
+    if len(C.shape) != 2:
+        return None
+
+    if not isinstance(X, np.ndarray):
+        return None
+
+    if C.shape[1] != X.shape[1]:
+        return None
+
+    if C.shape[0] < 1:
+        return None
+
+    return (np.sum(np.sum(np.min(np.linalg.norm(X[:, :, np.newaxis] -
+                                                C[:, :, np.newaxis]
+                                                .T, axis=1),
+                                 axis=1) ** 2)))
