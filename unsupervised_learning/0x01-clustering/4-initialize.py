b6d61f7056a394a432e5eb54f9af9fe1804c0c47
JohnCook17 <655@holbertonschool.com>
2020-12-27 20:40:29 +0000

    4-initialize.py

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-""""""
+"""Init gmm"""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
-    """"""
+    """uses kmeans to init gaussian mixture model"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+
     pi = np.array(np.repeat((1 / k), k))
-    m = kmeans(X, k)
+    m = kmeans(X, k)[0]
     identity_m = np.identity(X.shape[1])
-    print(identity_m)
+    # print(identity_m)
     S = np.repeat(identity_m[np.newaxis, ...], k, axis=0)
 
     return pi, m, S
