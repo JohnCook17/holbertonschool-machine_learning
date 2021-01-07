308d1b052d59eba4157e201c64df834dec4e7a8c
JohnCook17 <655@holbertonschool.com>
2021-01-07 18:22:09 +0000

    mask should work now

--- supervised_learning/0x11-attention/5-sdp_attention.py
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -9,7 +9,7 @@ def sdp_attention(Q, K, V, mask=None):
     attn = tf.matmul(Q, K, transpose_b=True)
 
     if mask is not None:
-        attn = tf.add(tf.multiply(mask(attn), -1e9), attn)
+        attn += (mask * -1e9)
 
     attn = tf.keras.layers.Softmax(axis=-1)(attn)
 
