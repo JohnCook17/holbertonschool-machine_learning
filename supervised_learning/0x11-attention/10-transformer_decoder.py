d65d643223187c0de5779e1a725fb28a1af9c284
JohnCook17 <655@holbertonschool.com>
2021-01-08 20:39:10 +0000

    task 10 and 11

---
+++ supervised_learning/0x11-attention/10-transformer_decoder.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Transformer Decoder"""
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
+
+
+class Decoder(tf.keras.layers.Layer):
+    """The Decoder for the transformer consisting of N blocks"""
+    def __init__(self,
+                 N,
+                 dm,
+                 h,
+                 hidden,
+                 target_vocab,
+                 max_seq_len,
+                 drop_rate=0.1):
+        """"""
+        super(Decoder, self).__init__()
+
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, self.dm)
+        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """Calls N blocks of decoder"""
+        seq_len = x.shape[1]
+
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:self.dm, :seq_len, None].T
+
+        x = self.dropout(x, training=training)
+
+        for i in range(self.N):
+            x = self.blocks[i](x,
+                               encoder_output,
+                               training,
+                               look_ahead_mask,
+                               padding_mask)
+
+        return x
---
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""The Transformer Model"""
+import tensorflow as tf
+Encoder = __import__('9-transformer_encoder').Encoder
+Decoder = __import__('10-transformer_decoder').Decoder
+
+
+class Transformer(tf.keras.Model):
+    """The Transformer model class"""
+    def __init__(self,
+                 N,
+                 dm,
+                 h,
+                 hidden,
+                 input_vocab,
+                 target_vocab,
+                 max_seq_input,
+                 max_seq_target,
+                 drop_rate=0.1):
+        """init of transformer"""
+        super(Transformer, self).__init__()
+
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab, max_seq_input, drop_rate)
+
+        self.decoder = Decoder(N, dm, h, hidden, target_vocab, max_seq_target, drop_rate)
+
+        self.linear = tf.keras.layers.Dense(units=target_vocab)
+
+    def call(self, inputs, target, training, encoder_mask, look_ahead_mask, decoder_mask):
+        """Calls the transformer"""
+        enc_output = self.encoder(inputs, training, encoder_mask)
+
+        dec_output = self.decoder(target, enc_output, training, look_ahead_mask, decoder_mask)
+
+        final_output = self.linear(dec_output)
+
+        return final_output
