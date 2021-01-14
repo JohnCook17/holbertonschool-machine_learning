5359b8717ecdaba82eacd7440c3684e5a64e8d83
JohnCook17 <655@holbertonschool.com>
2021-01-14 23:01:11 +0000

    rework

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -5,17 +5,24 @@ import tensorflow.compat.v2 as tf
 
 def create_masks(inputs, target):
     """Creates 3 mask"""
-    enc_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
-    enc_mask = enc_mask[:, tf.newaxis, tf.newaxis, :]
 
-    batch_size = target.shape.as_list()[0]
-    la_size = target.shape.as_list()[1]
-    la_mask = 1 - tf.linalg.band_part(tf.ones((batch_size,
-                                               la_size,
-                                               la_size)), -1, 0)
-    la_mask = la_mask[:, tf.newaxis, :, :]
+    def create_padding_mask(seq):
+        """creates a padding mask"""
+        seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
 
-    dec_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
-    dec_mask = dec_mask[:, tf.newaxis, tf.newaxis, :]
+        return seq[: tf.newaxis, tf.newaxis, :]
 
-    return enc_mask, la_mask, dec_mask
+    def create_look_ahead_mask(size):
+        """creates la mask"""
+        mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+        return mask
+
+    enc_mask = create_padding_mask(inputs)
+
+    dec_mask = create_padding_mask(inputs)
+
+    la_mask = create_la_mask(tf.shape(target)[1])
+    dec_tar_mask = create_padding_mask(tar)
+    combined_mask = tf.maximum(dec_tar_mask, la_mask)
+
+    return enc_mask, combined_mask, dec_mask
