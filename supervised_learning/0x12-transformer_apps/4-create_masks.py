ab55f9e7707a6b6fa0f42559cbbd17e3a25dd1d5
JohnCook17 <655@holbertonschool.com>
2021-01-14 21:29:42 +0000

    creates needed mask

---
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Mask so that null values of padding are not accounted"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+def create_masks(inputs, target):
+    """Creates 3 mask"""
+    enc_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    enc_mask = enc_mask[:, tf.newaxis, tf.newaxis, :]
+
+    la_size = tf.size(target)
+    la_mask = 1 - tf.linalg.band_part(tf.ones((la_size, la_size)), -1, 0)
+
+    dec_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    dec_mask = dec_mask[:, tf.newaxis, tf.newaxis, :]
+
+    return enc_mask, la_mask, dec_mask
