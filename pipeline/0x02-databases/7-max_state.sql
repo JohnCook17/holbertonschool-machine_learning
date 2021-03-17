0c14250858cac38fb397c1585dd74c548b29f78a
JohnCook17 <655@holbertonschool.com>
2021-03-17 12:52:37 -0700

    column name

--- pipeline/0x02-databases/7-max_state.sql
+++ pipeline/0x02-databases/7-max_state.sql
@@ -1,3 +1,3 @@
 -- max temp by state name
 
-SELECT state, MAX(value) FROM temperatures GROUP BY state ORDER BY state ASC;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
