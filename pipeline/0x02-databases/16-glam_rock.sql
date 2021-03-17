a6fbfe7f82912bf8996d6efb81e484a4a7d66ae1
JohnCook17 <655@holbertonschool.com>
2021-03-17 13:02:19 -0700

    using 2020 instead of curdate

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -1,3 +1,3 @@
 -- list all glam rock bands by longevity
 
-SELECT `band_name`, IFNULL(`split`, YEAR(CURDATE())) - `formed` AS `lifespan` FROM metal_bands WHERE `style` LIKE '%Glam rock%' ORDER BY `lifespan` DESC;
+SELECT band_name, IFNULL(split, 2020) - formed AS lifespan FROM metal_bands WHERE `style` LIKE '%Glam rock%' ORDER BY `lifespan` DESC;
