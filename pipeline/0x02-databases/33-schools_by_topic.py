c442ddfc2d5264e03dc4678dda4a1fc456084241
JohnCook17 <655@holbertonschool.com>
2021-03-17 18:33:14 -0700

    docs, semi colon, quotes

--- pipeline/0x02-databases/33-schools_by_topic.py
+++ pipeline/0x02-databases/33-schools_by_topic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""""""
+"""Finds topics in the collection"""
 
 
 def schools_by_topic(mongo_collection, topic):
-    """"""
+    """finds the desiered topic"""
     return mongo_collection.find({"topics": topic})
